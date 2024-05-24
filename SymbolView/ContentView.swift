6d8966ccf45c05bd488e9484bb8c6fa52d80ecac
Dalton Alexandre <166029845+dl-alexandre@users.noreply.github.com>
2024-05-24 15:23:22 -0700

    Initial Commit

---
+++ SymbolView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SymbolView
+//
+//  Created by Dalton Alexandre on 5/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SymbolView/SymbolViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolViewApp.swift
+//  SymbolView
+//
+//  Created by Dalton Alexandre on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SymbolViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
