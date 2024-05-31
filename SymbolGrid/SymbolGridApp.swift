c550d2d2ad1b3e30f2295cbd6503c3d2cbcf7c67
Dalton Alexandre <166029845+dl-alexandre@users.noreply.github.com>
2024-05-31 10:54:33 -0700

    Initial Commit

---
+++ SymbolGrid/SymbolGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolGridApp.swift
+//  SymbolGrid
+//
+//  Created by Dalton Alexandre on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct SymbolGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
